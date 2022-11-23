731253a332dc13ed8e5ae6b640c527440423bb9d
Yasuhiro Usutani <y_usutani@mac.com>
2022-11-23 16:32:36 +0900

    3

--- app/controllers/retentions_controller.rb
+++ app/controllers/retentions_controller.rb
@@ -1,4 +1,31 @@
 class RetentionsController < ApplicationController
+  before_action :set_retention, only: %i[ show update ]
+
   def show
+    @items = [nil, 30, 90, 365, 730]
   end
+
+  def update
+    if confirmed? && @retention.update(retention_params)
+      redirect_to home_url, notice: 'Retentions successfully saved.'
+    else
+      @retention = Retention.new(retention_params)
+      render status: :unprocessable_entity
+    end
+  end
+
+  private
+    def set_retention
+      @retention = Retention.first!
+    end
+
+    def retention_params
+      params.slice(:cycle, :project)
+            .permit!
+            .to_h { |k, v| [k, v.presence] }
+    end
+
+    def confirmed?
+      params[:confirmation] == 'CONFIRM'
+    end
 end
--- app/helpers/retentions_helper.rb
+++ app/helpers/retentions_helper.rb
@@ -1,2 +1,9 @@
 module RetentionsHelper
+  def days_to_human(days)
+    return 'Forever' if days.blank?
+    years = days.to_i / 365
+    return "#{days} days" if years == 0
+    return "#{years} year" if years == 1
+    "#{years} years"
+  end
 end
